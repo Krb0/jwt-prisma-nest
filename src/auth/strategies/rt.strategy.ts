497d9cf7a3927e0067e453a2afe032b94382925b
krb0 <maurodavidcaceres@gmail.com>
2022-05-30 02:53:51 -0300

    tokens strategies added

---
+++ src/auth/strategies/rt.strategy.ts
@@ -0,0 +1,15 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+export class RtStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.RT_SECRET,
+      passReqToCallback: true,
+    });
+  }
+  validate(req: Request, payload: any) {
+    return payload;
+  }
+}
