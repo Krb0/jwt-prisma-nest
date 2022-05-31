0a997cb072feb8247b307029deac4b75388c23ea
krb0 <maurodavidcaceres@gmail.com>
2022-05-31 03:03:00 -0300

    guard added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { AuthModule } from './auth/auth.module';
+import { AtGuard } from './common/guards';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
 @Module({
-  imports: [AuthModule, PrismaModule],
+  imports: [AuthModule, PrismaModule, UserModule],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AtGuard,
+    },
+  ],
 })
 export class AppModule {}
